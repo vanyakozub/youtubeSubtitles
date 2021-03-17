36bc02b050cbf7627ea171b6eb0facf11f9cd90a
vanyakozub <59049483+vanyakozub@users.noreply.github.com>
2021-03-17 08:31:14 +0300

    Add files via upload

--- com.kozub.youtubeSubtitles/src/main/java/ApiExample.java
+++ com.kozub.youtubeSubtitles/src/main/java/ApiExample.java
@@ -1,77 +1,28 @@
-/**
- * Sample Java code for youtube.captions.download
- * See instructions for running these code samples locally:
- * https://developers.google.com/explorer-help/guides/code_samples#java
- */
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Caption;
 import com.google.api.services.youtube.model.CaptionListResponse;
 import com.google.api.services.youtube.model.CaptionSnippet;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import opennlp.tools.tokenize.SimpleTokenizer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.morphology.russian.RussianAnalyzer;
+
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ApiExample {
-    private static final String CLIENT_SECRETS= "client_secret.json";
-    private static final Collection<String> SCOPES =
-            Arrays.asList("https://www.googleapis.com/auth/youtube.force-ssl");
 
-    private static final String APPLICATION_NAME = "API code samples";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-    /**
-     * Create an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize(final NetHttpTransport httpTransport) throws Exception {
-        // Load client secrets.
-        
-
-        InputStream in = ApiExample.class.getResourceAsStream(CLIENT_SECRETS);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-                        .build();
-        Credential credential =
-                new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-        return credential;
-    }
-
-    /**
-     * Build and return an authorized API client service.
-     *
-     * @return an authorized API client service
-     * @throws GeneralSecurityException, IOException
-     */
-    public static YouTube getService() throws GeneralSecurityException, Exception {
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = authorize(httpTransport);
-        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
 
     /**
      * Call function to create API service object. Define and
@@ -81,35 +32,111 @@ public class ApiExample {
      */
     public static void main(String[] args)
             throws GeneralSecurityException, Exception, GoogleJsonResponseException {
-        YouTube youtubeService = getService();
-        String videoId ="fJ9rUzIMcZQ";
-        // TODO: Replace "YOUR_FILE" with the location where
-        //       the downloaded content should be written.
-        OutputStream output = new FileOutputStream("sub.txt");
 
+        /**
+         * work
+         */
+
+       /* YouTube youtubeService = SubExtractor.getService();
+        String videoId =args[0].substring(args[0].indexOf("v="));
+        videoId = videoId.substring(2, videoId.length()-1);
+        OutputStream output = new FileOutputStream(args[2]);
         CaptionListResponse captionListResponse = youtubeService.captions().
                 list("snippet", videoId).execute();
 
         List<Caption> captions = captionListResponse.getItems();
         // Print information from the API response.
-        System.out.println("\n============ Returned Caption Tracks ============\n");
-        CaptionSnippet snippet;
-        String trackId = new String();
-        for (Caption caption : captions) {
-            snippet = caption.getSnippet();
-            System.out.println("  - ID: " + caption.getId());
-            trackId = caption.getId();
-            System.out.println("  - Name: " + snippet.getName());
-            System.out.println("  - Language: " + snippet.getLanguage());
-            System.out.println("\n----------------------------------------------\n");
-        }
-        // Define and execute the API request
+        String trackId ="";
+        Caption caption = captions.get(0);
+        trackId = caption.getId();
+
         YouTube.Captions.Download request = youtubeService.captions()
                 .download(trackId);
-        //HttpGet req = new HttpGet("www.googleapis.com/youtube/v3/captions/fJ9rUzIMcZQ");
-        //CloseableHttpClient client = HttpClients.createDefault();
-        //CloseableHttpResponse response = client.execute(req);
         request.getMediaHttpDownloader();
-        request.executeMediaAndDownloadTo(output);
+        request.executeMediaAndDownloadTo(output);/*
+
+        /**
+         * до сюда
+         */
+        if ("text".equals(args[1])) {
+
+
+            Stream<String> wordsStream = Files.lines(Paths.get(args[2]));
+
+            Object[] content = wordsStream.toArray();
+
+            ArrayList<String> subtitles = new ArrayList<>();
+            ArrayList<Timing> timming = new ArrayList<>();
+
+            String tmpBegin = "";
+            String tmpEnd = "";
+            String tmpHours = "";
+            String tmpMinutes = "";
+            String tmpSeconds = "";
+            String tmpMilliseconds = "";
+            String[] tmp;
+            String[] tmpTimming1;
+            String[] tmpTimming2;
+            for (int i = 0; i < content.length - 1; i++) {
+                if (i % 3 == 1) {
+                    subtitles.add((String) content[i]);
+                }
+                if (i % 3 == 0) {
+                    tmp = content[i].toString().split(",");
+                    tmpBegin = tmp[0];
+                    tmpEnd = tmp[1];
+                    tmpTimming1 = tmpBegin.split(":");
+                    tmpTimming2 = tmpEnd.split(":");
+                    Duration begin = Duration.ofHours(Integer.parseInt(tmpTimming1[0]));
+                    begin = begin.plusMinutes(Integer.parseInt(tmpTimming1[1]));
+                    begin = begin.plusSeconds(Integer.parseInt(tmpTimming1[2].substring(0, tmpTimming1[2].indexOf("."))));
+                    begin = begin.plusMillis(Integer.parseInt(tmpTimming1[2].substring(tmpTimming1[2].indexOf(".") + 1)));
+                    Duration end = Duration.ofHours(Integer.parseInt(tmpTimming2[0]));
+                    end = end.plusMinutes(Integer.parseInt(tmpTimming2[1]));
+                    end = end.plusSeconds(Integer.parseInt(tmpTimming2[2].substring(0, tmpTimming2[2].indexOf("."))));
+                    end = end.plusMillis(Integer.parseInt(tmpTimming2[2].substring(tmpTimming2[2].indexOf(".") + 1)));
+                    timming.add(new Timing(begin, end));
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("TEXT.txt"));
+            writer.append("\t");
+            for (int i = 0; i < timming.size() - 1; i++) {
+                long curDelay = timming.get(i).getEnd().toMillis() - timming.get(i + 1).getBegin().toMillis();
+                System.out.println(curDelay);
+                if (i % 6 == 0 && i > 0) {
+                    writer.newLine();
+                }
+                writer.append(subtitles.get(i));
+                if (curDelay < 0) {
+                    writer.append(".");
+                    writer.newLine();
+                    writer.newLine();
+                    writer.append("\t");
+                }
+                if (curDelay > 3300) {
+                    writer.append(".");
+                }
+                writer.append(" ");
+            }
+            writer.append(subtitles.get(subtitles.size() - 1));
+
+
+
+            for (Object str : content) {
+                //System.out.println(str);
+            }
+            System.out.println(subtitles);
+            System.out.println(timming);
+        }
+        RussianAnalyzer analyzer = new RussianAnalyzer();
+        /*TokenStream stream = analyzer.tokenStream("field", content);
+        stream.reset();
+        while (stream.incrementToken()) {
+            String lemma = stream.getAttribute(CharTermAttribute.class).toString();
+            System.out.print(lemma + " ");
+        }
+        stream.end();
+        stream.close();*/
     }
 }
